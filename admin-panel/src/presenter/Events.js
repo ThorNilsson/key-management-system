861b987245405dc3f713866cf66f9191b5051294
Kalle-Elmdahl <75391187+Kalle-Elmdahl@users.noreply.github.com>
2022-04-14 11:37:25 +0200

    Cleanup

--- admin-panel/src/presenter/Events.js
+++ admin-panel/src/presenter/Events.js
@@ -1,16 +1,8 @@
 import React from 'react';
-import EventsView from './EventsView';
-import { initializeApp } from 'firebase/app';
-import { getFirestore, collection, getDocs } from 'firebase/firestore/lite';
-import { getDatabase, ref, onValue } from "firebase/database";
+/* import EventsView from './EventsView'; */
 
 export default function EventsPresenter(props) {
 
-    /** Sortera alla objekt efter deras datum.
-     *  props.keys.sort(function(a, b) {
-        return parseFloat(b.time) - parseFloat(a.time);
-        });
-     */
     const data = [
         {
             "bookingId": "",
@@ -39,34 +31,7 @@ export default function EventsPresenter(props) {
         }
     ]
 
-    return (
+    return null/* (
         <EventsView keys={data}></EventsView>
-    )
-}
-
-function fetchData() {
-    let keys = null;
-    const firebaseConfig = {
-        apiKey: "AIzaSyAUsPBPy1B5cr_U0xeB1xPU8T_7S-x_dyg",
-        authDomain: "key-management-system-40057.firebaseapp.com",
-        databaseURL: "https://key-management-system-40057-default-rtdb.europe-west1.firebasedatabase.app",
-        projectId: "key-management-system-40057",
-        storageBucket: "key-management-system-40057.appspot.com",
-        messagingSenderId: "818193873576",
-        appId: "1:818193873576:web:2e3c62dffff7d584f7a09d"
-    };
-
-    const app = initializeApp(firebaseConfig);
-    const db = getDatabase(app);
-
-    const logs = ref(db, 'keyboxes/dkgC3kfhLpkKBysY_C-9/log');
-    onValue(logs, (snapshot) => {
-        const data = snapshot.val();
-        keys = Object.values(data);
-        keys.sort(function (a, b) {
-            return parseFloat(b.time) - parseFloat(a.time);
-        });
-        console.log(keys);
-        return keys;
-    });
+    ) */
 }
