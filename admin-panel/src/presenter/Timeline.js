ee87e7da397c7d854255eb4f325ff75e109b828b
NicoleWij <nicole.wijkman@gmail.com>
2022-04-14 10:04:22 +0200

    Created Calendar
    Created a first version of the timeline calendar.

--- admin-panel/src/presenter/Timeline.js
+++ admin-panel/src/presenter/Timeline.js
@@ -1,6 +1,98 @@
-export default function TimelinePresenter() {
+import TimelineView from "../view/TimelineView.js";
 
+// make sure you include the timeline stylesheet or the timeline will not be styled
+import 'react-calendar-timeline/lib/Timeline.css'
+import moment from 'moment'
+
+// npm install --save react-calendar-timeline
+
+const groups = [
+    {
+        id: 1,
+        title: 'Key 1'
+    },
+    {
+        id: 2,
+        title: 'Key 2'
+    },
+    {
+        id: 3,
+        title: 'Key 3'
+    },
+    {
+        id: 4,
+        title: 'Key 4'
+    }]
+
+const items = [
+    {
+        id: 1,
+        group: 1,
+        start_time: moment().add(1, 'day').hours(0).minutes(0).seconds(0),
+        end_time: moment().add(15, 'day').hours(6).minutes(0).seconds(0),
+        itemProps: {
+            // these optional attributes are passed to the root <div /> of each item as <div {...itemProps} />
+            'data-custom-attribute': 'Random content',
+            'aria-hidden': true,
+            onDoubleClick: () => { console.log('You clicked double!') },
+            className: 'weekend',
+            style: {
+                background: 'grey'
+            }
+        }
+    },
+    {
+        id: 2,
+        group: 2,
+        start_time: moment().add(-1, 'day').hours(0).minutes(0).seconds(0),
+        end_time: moment().add(5, 'day').hours(0).minutes(0).seconds(0),
+        itemProps: {
+            // these optional attributes are passed to the root <div /> of each item as <div {...itemProps} />
+            'data-custom-attribute': 'Random content',
+            'aria-hidden': true,
+            onDoubleClick: () => { console.log('You clicked double!') },
+            className: 'weekend',
+            style: {
+                background: 'grey'
+            }
+        }
+    },
+    {
+        id: 3,
+        group: 3,
+        start_time: moment().add(2, 'day').hours(0).minutes(0).seconds(0),
+        end_time: moment().add(9, 'day').hours(0).minutes(0).seconds(0),
+        itemProps: {
+            // these optional attributes are passed to the root <div /> of each item as <div {...itemProps} />
+            'data-custom-attribute': 'Random content',
+            'aria-hidden': true,
+            onDoubleClick: () => { console.log('You clicked double!') },
+            className: 'weekend',
+            style: {
+                background: 'grey'
+            }
+        }
+    },
+    {
+        id: 4,
+        group: 4,
+        start_time: moment().add(-15, 'day').hours(0).minutes(0).seconds(0),
+        end_time: moment().add(7, 'day').hours(0).minutes(0).seconds(0),
+        itemProps: {
+            // these optional attributes are passed to the root <div /> of each item as <div {...itemProps} />
+            'data-custom-attribute': 'Random content',
+            'aria-hidden': true,
+            onDoubleClick: () => { console.log('You clicked double!') },
+            className: 'weekend',
+            style: {
+                background: 'grey'
+            }
+        }
+    }
+]
+
+export default function TimelinePresenter() {
     return (
-        <div>TimelinePresenter</div>
+        <TimelineView groups={groups} items={items}/>
     )
 }
---
+++ admin-panel/src/view/TimelineView.js
@@ -0,0 +1,42 @@
+import React from 'react';
+import 'react-calendar-timeline/lib/Timeline.css';
+import moment from 'moment';
+import Timeline, {
+    TimelineHeaders,
+    SidebarHeader,
+    DateHeader
+  } from 'react-calendar-timeline'
+import { Typography } from '@mui/material';
+
+class TimelineView extends React.Component {
+    render() {
+        return (
+            <div className="Timeline">
+                <Typography variant="h3" sx={{mb: 3, mt: 3}}>Timeline of Key Status</Typography>
+                <h1> </h1>
+                <Timeline
+                    groups={this.props.groups}
+                    items={this.props.items}
+                    defaultTimeStart={moment().add(-15, 'day')}
+                    defaultTimeEnd={moment().add(15, 'day')}>
+                    <TimelineHeaders>
+                        <SidebarHeader>
+                            {({ getRootProps }) => {
+                                return <div {...getRootProps()}>Keys</div>
+                            }}
+                        </SidebarHeader>
+                        <SidebarHeader variant="right" headerData={{ someData: 'extra' }}>
+                            {({ getRootProps, data }) => {
+                                return <div {...getRootProps()}> Right {data.someData}</div>
+                            }}
+                        </SidebarHeader>
+                        <DateHeader unit="primaryHeader" />
+                        <DateHeader />
+                    </TimelineHeaders>
+                </Timeline>
+            </div>
+        );
+    }
+}
+
+export default TimelineView;
