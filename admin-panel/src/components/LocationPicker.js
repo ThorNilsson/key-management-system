72e5d71af94208e40cac8419d86c7fbb6b18d848
Kalle-Elmdahl <75391187+Kalle-Elmdahl@users.noreply.github.com>
2022-05-12 15:38:02 +0200

    Cleanup

--- admin-panel/src/components/LocationPicker.js
+++ admin-panel/src/components/LocationPicker.js
@@ -9,13 +9,11 @@ const pinAsSvgString =
 	'<svg xmlns="http://www.w3.org/2000/svg" enable-background="new 0 0 24 24" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><g><rect fill="none" height="24" width="24" y="0"/></g><g><path d="M12,2c-4.2,0-8,3.22-8,8.2c0,3.18,2.45,6.92,7.34,11.23c0.38,0.33,0.95,0.33,1.33,0 C17.55,17.12,20,13.38,20,10.2C20,5.22,16.2,2,12,2z M12,12c-1.1,0-2-0.9-2-2c0-1.1,0.9-2,2-2c1.1,0,2,0.9,2,2 C14,11.1,13.1,12,12,12z" enable-background="new"/></g></svg>'
 
 export default function LocationPicker({ lng, lat, setLng, setLat, sx, ...props }) {
-    console.log(lng + " " + lat)
 	const mapContainer = useRef(null)
 	const map = useRef(null)
 	const marker = useRef(null)
 
 	const setMarker = (lng, lat) => {
-        console.log(marker.current, lng, lat)
 		if (marker.current) return marker.current.setLngLat([lng, lat])
 
 		const div = document.createElement("div")
@@ -44,15 +42,13 @@ export default function LocationPicker({ lng, lat, setLng, setLat, sx, ...props 
 		if (map.current) return
 
 		if (!lng || !lat) {
-            console.log("Here")
 			navigator.geolocation.getCurrentPosition(
 				({ coords }) => createMap(coords.longitude, coords.latitude, 5),
 				() => createMap(0, 0, -5)
 			)
 		} else {
-            console.log(lng, lat)
-			createMap(Number(lng), Number(lat), 8)
-			setMarker(Number(lng), Number(lat))
+			createMap(lng, lat, 8)
+			setMarker(lng, lat)
 		}
 	})
 
