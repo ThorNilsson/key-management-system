b6a684aa07df70c609e859d3dc3d1789009ab7e2
ThorNilsson <thor756nilsson@gmail.com>
2022-05-13 19:56:29 +0200

    Drag feature on timeline, on double click new booking

--- admin-panel/src/presenter/Timeline.js
+++ admin-panel/src/presenter/Timeline.js
@@ -3,38 +3,89 @@ import TimelineView from "../view/TimelineView.js"
 import "react-calendar-timeline/lib/Timeline.css"
 import moment from "moment"
 
-import { useList } from "react-firebase-hooks/database"
-import { ref } from "firebase/database"
-import { db } from "../api/firebase.js"
+import {useList} from "react-firebase-hooks/database"
+import {push, ref, set} from "firebase/database"
+import {db} from "../api/firebase.js"
 
-import { useParams } from "react-router-dom"
+import {useNavigate, useParams} from "react-router-dom"
+import {getHours, getMinutes, setHours, setMinutes} from "date-fns";
+import {useState} from "react";
 
 export default function TimelinePresenter() {
-	const { boxId } = useParams()
+    const {boxId} = useParams()
+    const navigate = useNavigate()
 
-	const [bookings, loadingBookings, errorBookings] = useList(ref(db, `keyboxes/${boxId}/bookings`))
-	const [keys, loadingKeys, errorKeys] = useList(ref(db, `keyboxes/${boxId}/keys`))
+    const [bookings, loadingBookings, errorBookings] = useList(ref(db, `keyboxes/${boxId}/bookings`))
+    const [keys, loadingKeys, errorKeys] = useList(ref(db, `keyboxes/${boxId}/keys`))
 
-	if (errorBookings || errorKeys) return <div>Something went wrong</div>
+    const [selectedBooking, setSelectedBooking] = useState({});
 
-	const groups = keys
-		.map(key => ({ ...key.val(), id: key.key }))
-		.map(({ name, id }) => ({
-			id,
-			title: name,
-		}))
-        
-	const items = bookings
-		.map(booking => ({ ...booking.val(), id: booking.key }))
-		.map(({ id, keyId, checkIn, checkOut }) => ({
-			id,
-			group: keyId,
-			start_time: moment(checkIn),
-			end_time: moment(checkOut),
-			canMove: false,
-		}))
 
-	console.log(items)
+    if (errorBookings || errorKeys) return <div>Something went wrong</div>
 
-	return <TimelineView groups={groups} items={items} loading={loadingBookings || loadingKeys} />
+    const groups = keys
+        .map(key => ({...key.val(), id: key.key}))
+        .map(({name, id}) => ({
+            id,
+            title: name,
+        }))
+
+
+    const items = bookings
+        .map(booking => ({...booking.val(), id: booking.key}))
+        .map(({id, keyId, checkIn, checkOut, name, email, message}) => ({
+            id,
+            group: keyId,
+            title: name,
+            start_time: moment(checkIn * 1000),
+            end_time: moment(checkOut * 1000),
+            email,
+            message,
+        }))
+
+    const handleBookingMove = (bookingId, newStartTime, key) => {
+        const booking = items.find(booking => booking.id === bookingId);
+
+        const updatedBooking = {
+            checkIn: new moment(newStartTime).set({
+                hour: booking.start_time.hour(),
+                minute: booking.start_time.minute()
+            }).unix(),
+            checkOut: new moment(newStartTime).add(booking.end_time.diff(booking.start_time, 'days'), "day").set({
+                hour: booking.end_time.hour(),
+                minute: booking.end_time.minute()
+            }).unix(),
+            email: booking.email,
+            keyId: key.id,
+            message: booking.message,
+            name: booking.title,
+        };
+
+        set(ref(db, "keyboxes/" + boxId + "/bookings/" + bookingId), updatedBooking)
+            .then(() => {
+            })
+            .catch(error => alert("Something went wrong " + error.message))
+    }
+
+    const handleSelectingBooking = (itemId) => {
+        let sel = items.find(booking => booking.id === itemId);
+        setSelectedBooking({
+            ...sel,
+            start_time: sel.start_time.format("hh:mm"),
+            end_time: sel.end_time.format("hh:mm"),
+        });
+    }
+
+    const handleNewBooking = (keyId, time) => {
+        const startDate = time;
+
+        console.log({keyId,startDate})
+        navigate(`/${boxId}/new-booking`);
+    }
+
+    console.log(items)
+
+    return <TimelineView groups={groups} items={items} loading={loadingBookings || loadingKeys}
+                         handleBookingMove={handleBookingMove} selectedBooking={selectedBooking}
+                         handleSelectingBooking={handleSelectingBooking} handleNewBooking={handleNewBooking}/>
 }
--- admin-panel/src/view/TimelineView.js
+++ admin-panel/src/view/TimelineView.js
@@ -7,27 +7,66 @@ import Timeline, {
     DateHeader,
     TodayMarker
 } from 'react-calendar-timeline'
-import { Typography } from '@mui/material';
+import {Typography, Chip, Grid} from '@mui/material';
+
 
 class TimelineView extends React.Component {
     render() {
         return (
             <div className="Timeline">
-                <Typography variant="h3" sx={{ my: 3 }}>Timeline of Key box</Typography>
+                <Grid container spacing={0.5}>
+                    <Grid>
+                        <Typography variant="h3" sx={{my: 3}}>Timeline of Key box</Typography>
+                    </Grid>
+                    {
+                        this.props.selectedBooking.start_time === undefined ? null : <>
+                            <Grid>
+                                <Chip label={"Check in: " + this.props.selectedBooking.start_time}/>
+                            </Grid>
+                            <Grid>
+                                <Chip label={"Check out: " + this.props.selectedBooking.end_time}/>
+                            </Grid>
+                            <Grid>
+                                <Chip label={this.props.selectedBooking.message} color={'primary'}/>
+                            </Grid>
+                        </>
+                    }
+                </Grid>
+
                 <Timeline
                     groups={this.props.groups}
                     items={this.props.items}
+                    minZoom={12 * 60 * 60 * 1000} //12h
+                    stackItems={true}
+                    canMove={true}
+                    canChangeGroup={true}
+                    canResize={false}
                     defaultTimeStart={moment().add(-15, 'day')}
-                    defaultTimeEnd={moment().add(15, 'day')}>
-                    <TodayMarker interval={2000} />
+                    defaultTimeEnd={moment().add(15, 'day')}
+                    dragSnap={60 * 24 * 60 * 1000}
+                    lineHeight={40}
+                    itemHeightRatio={0.8}
+                    clickTolerance={1}
+                    onCanvasDoubleClick={(groupId, time, e) => {
+                        this.props.handleNewBooking(groupId, time)
+                        console.log({groupId, time, e})
+                    }}
+                    onItemSelect={(itemId, time, e) => {
+                        this.props.handleSelectingBooking(itemId);
+                    }}
+                    onItemMove={(itemId, dragTime, newGroupOrder) => {
+                        this.props.handleBookingMove(itemId, dragTime, this.props.groups[newGroupOrder]);
+                    }}
+                >
+                    <TodayMarker interval={2000}/>
                     <TimelineHeaders>
-                        <SidebarHeader headerData={{ someData: 'extra' }}>
-                            {({ getRootProps }) => {
+                        <SidebarHeader headerData={{someData: 'extra'}}>
+                            {({getRootProps}) => {
                                 return <Typography variant="h3" color="white" {...getRootProps()}>Keys</Typography>
                             }}
                         </SidebarHeader>
-                        <DateHeader unit="primaryHeader" />
-                        <DateHeader />
+                        <DateHeader unit="primaryHeader"/>
+                        <DateHeader/>
                     </TimelineHeaders>
                 </Timeline>
             </div>
