856dca7bfdd90f517e54f2d535d1c3aa2c68f3bf
ThorNilsson <thor756nilsson@gmail.com>
2022-05-15 00:48:47 +0200

    Tried to fix bug

--- admin-panel/src/view/TimelineView.js
+++ admin-panel/src/view/TimelineView.js
@@ -7,69 +7,87 @@ import Timeline, {
     DateHeader,
     TodayMarker
 } from 'react-calendar-timeline'
-import {Typography, Chip, Grid} from '@mui/material';
-
+import {Typography, Chip, Button, Stack,Skeleton} from '@mui/material';
+import DeleteIcon from '@mui/icons-material/Delete';
 
 class TimelineView extends React.Component {
     render() {
         return (
-            <div className="Timeline">
-                <Grid container spacing={0.5}>
-                    <Grid>
-                        <Typography variant="h3" sx={{my: 3}}>Timeline of Key box</Typography>
-                    </Grid>
+            <>
+                <Typography variant="h3" sx={{my: 3}}>Timeline of Key box</Typography>
+                <Stack direction="row" spacing={2}>
                     {
-                        this.props.selectedBooking.start_time === undefined ? null : <>
-                            <Grid>
-                                <Chip label={"Check in: " + this.props.selectedBooking.start_time}/>
-                            </Grid>
-                            <Grid>
-                                <Chip label={"Check out: " + this.props.selectedBooking.end_time}/>
-                            </Grid>
-                            <Grid>
-                                <Chip label={this.props.selectedBooking.message} color={'primary'}/>
-                            </Grid>
-                        </>
-                    }
-                </Grid>
+                        this.props.selectedBooking.start_time === undefined ?
+                            <>
+                                <Skeleton variant="rectangular" width={210} height={20}/>
+                                {
+                                //<Skeleton variant="text"/>
+                                //<Skeleton variant="circular" width={40} height={40}/>
+                                //<Skeleton variant="rectangular" width={210} height={118}/>
+                                }
+                            </> :
+                            <>
 
-                <Timeline
-                    groups={this.props.groups}
-                    items={this.props.items}
-                    minZoom={12 * 60 * 60 * 1000} //12h
-                    stackItems={true}
-                    canMove={true}
-                    canChangeGroup={true}
-                    canResize={false}
-                    defaultTimeStart={moment().add(-15, 'day')}
-                    defaultTimeEnd={moment().add(15, 'day')}
-                    dragSnap={60 * 24 * 60 * 1000}
-                    lineHeight={40}
-                    itemHeightRatio={0.8}
-                    clickTolerance={1}
-                    onCanvasDoubleClick={(groupId, time, e) => {
-                        this.props.handleNewBooking(groupId, time)
-                        console.log({groupId, time, e})
-                    }}
-                    onItemSelect={(itemId, time, e) => {
-                        this.props.handleSelectingBooking(itemId);
-                    }}
-                    onItemMove={(itemId, dragTime, newGroupOrder) => {
-                        this.props.handleBookingMove(itemId, dragTime, this.props.groups[newGroupOrder]);
-                    }}
-                >
-                    <TodayMarker interval={2000}/>
-                    <TimelineHeaders>
-                        <SidebarHeader headerData={{someData: 'extra'}}>
-                            {({getRootProps}) => {
-                                return <Typography variant="h3" color="white" {...getRootProps()}>Keys</Typography>
-                            }}
-                        </SidebarHeader>
-                        <DateHeader unit="primaryHeader"/>
-                        <DateHeader/>
-                    </TimelineHeaders>
-                </Timeline>
-            </div>
+                                <Chip label={"Check in: " + this.props.selectedBooking.start_time}/>
+
+                                <Chip label={"Check out: " + this.props.selectedBooking.end_time}/>
+
+                                <Chip label={this.props.selectedBooking.message} color={'primary'}/>
+
+                                <Button variant="outlined" startIcon={<DeleteIcon/>}
+                                    onClick={this.props.handleBookingDelete}
+                                >
+                                    Delete
+                                </Button>
+                            </>
+                    }
+                </Stack>
+                <div className="Timeline">
+                    <Timeline
+                        groups={this.props.groups}
+                        items={this.props.items}
+                        defaultTimeStart={moment().subtract(7, "days")}
+                        defaultTimeEnd={moment().add(14, "days")}
+                        minZoom={12 * 60 * 60 * 1000} //12h
+                        stackItems={true}
+                        canMove={true}
+                        canChangeGroup={true}
+                        canResize={false}
+                        traditionalZoom={true}
+                        dragSnap={60 * 6 * 60 * 1000}
+                        lineHeight={40}
+                        itemHeightRatio={0.8}
+                        clickTolerance={1}
+                        onCanvasClick={(groupId, time, e) => {
+                            this.props.handleDeSelectingBooking(groupId, time)
+                        }}
+                        onCanvasDoubleClick={(groupId, time, e) => {
+                            this.props.handleNewBooking(groupId, time)
+                            console.log({groupId, time, e})
+                        }}
+                        onItemSelect={(itemId, time, e) => {
+                            this.props.handleSelectingBooking(itemId);
+                        }}
+                        onItemMove={(itemId, dragTime, newGroupOrder) => {
+                            this.props.handleBookingMove(itemId, dragTime, this.props.groups[newGroupOrder]);
+                        }}
+                        onBoundsChange={(canvasTimeStart, canvasTimeEnd) => {
+                            console.log({canvasTimeStart, canvasTimeEnd})
+                        }}
+                    >
+                        <TodayMarker interval={2000}/>
+                        <TimelineHeaders>
+                            <SidebarHeader headerData={{someData: 'extra'}}>
+                                {({getRootProps}) => {
+                                    return <Typography variant="h3" color="white" {...getRootProps()}>Keys</Typography>
+                                }}
+                            </SidebarHeader>
+                            <DateHeader unit="primaryHeader"/>
+                            <DateHeader/>
+                        </TimelineHeaders>
+                    </Timeline>
+                </div>
+            </>
         );
     }
 }
