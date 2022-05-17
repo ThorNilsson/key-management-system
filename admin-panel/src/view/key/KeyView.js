60304970dfde76008b3a1266bcf3f202ab46c264
Kalle-Elmdahl <75391187+Kalle-Elmdahl@users.noreply.github.com>
2022-05-17 16:57:18 +0200

    Minor

--- admin-panel/src/view/key/KeyView.js
+++ admin-panel/src/view/key/KeyView.js
@@ -1,8 +1,10 @@
-import { CircularProgress, DialogContent, Typography, Button, DialogActions, CardMedia } from "@mui/material"
+import { CircularProgress, DialogContent, Typography, Button, DialogActions, CardMedia, Stack, Paper } from "@mui/material"
 import React from "react"
 import BoxPopupHeader from "../../components/BoxPopupHeader"
 
-import { ArrowForwardIosRounded, Key } from "@mui/icons-material"
+import { grey } from '@mui/material/colors';
+
+import { ArrowForwardIosRounded, ImageRounded, KeyRounded } from "@mui/icons-material"
 
 export default function KeyView({ edit, close, loading, info, release }) {
 	if (loading) return <CircularProgress />
@@ -11,18 +13,21 @@ export default function KeyView({ edit, close, loading, info, release }) {
 		<>
 			<BoxPopupHeader title={`Key for ${info.name}`} close={close} />
 			<DialogContent sx={{ width: 400 }}>
-				<CardMedia
-					component="img"
-					alt="No image"
-					height="140"
-					image={info.image}
-				/>
+				{info.image && info.image !== "" ? (
+					<CardMedia component="img" alt="No image" height="140" image={info.image} />
+				) : (
+					<Paper sx={{ backgroundColor: grey[200], mb: 3}}>
+						<Stack justifyContent="center" alignItems="center" sx={{ minHeight: 140 }}>
+							<ImageRounded sx={{ fontSize: 80 }} />
+						</Stack>
+					</Paper>
+				)}
 				<Typography variant="body1" sx={{ mb: 1 }}>
 					{info.description}
 				</Typography>
 			</DialogContent>
 			<DialogActions>
-				<Button onClick={release} variant="contained" color="error" endIcon={<Key />}>
+				<Button onClick={release} disabled={info.keySlot !== info.preferredKeySlot} variant="contained" color="error" endIcon={<KeyRounded />}>
 					Release key
 				</Button>
 				<Button onClick={edit} variant="outlined" endIcon={<ArrowForwardIosRounded />}>
