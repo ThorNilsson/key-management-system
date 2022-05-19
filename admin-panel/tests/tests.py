43fa31818f4a3058e828d7cb89fbe9f76724e3f8
MiniMarius <seffermarius@gmail.com>
2022-05-19 21:06:28 +0200

    tests now incorporated according to Unittest framework with separate tests and a working setup

--- admin-panel/tests/tests.py
+++ admin-panel/tests/tests.py
@@ -28,25 +28,10 @@ from selenium.webdriver.support.ui import Select
 class KMS_test_class(unittest.TestCase):
 
     # initialization of webdriver
+    # logs into the admin panel as a setup before each admin-panel test start
     def setUp(self):
         #self.driver = webdriver.Chrome()
         self.driver = webdriver.Chrome(ChromeDriverManager().install())
-
-
-    # Test case method. It should always start with test_
-    def tests(self):
-
-        # call login function
-        self.login_test()
-        self.new_booking_form_test()
-        # call overview function
-        #self.overview_test()
-        # call new booking test function
-        #self.new_booking_test()
-        #self.editbox_test()
-
-
-    def login_test(self):
         # get driver
         driver = self.driver
         # get python.org using selenium
@@ -54,17 +39,15 @@ class KMS_test_class(unittest.TestCase):
         time.sleep(2)
         # locate element using name
         go_to_case = driver.find_element(by=By.XPATH, value="//*[@id='root']/main/div/form/div[4]/button").click()
-        time.sleep(2)
+        time.sleep(1)
         fill_in_email = driver.find_element(by=By.XPATH, value="//*[@id='email']").send_keys("mi.cho123@hotmail.com")
         fill_in_pass = driver.find_element(by=By.XPATH, value="//*[@id='password']").send_keys("abc123")
-
         press_login = driver.find_element(by=By.XPATH, value="//*[@id='root']/main/div/form/button").click()
         time.sleep(2)
         # press_visit //*[@id="root"]/div/div[2]/div/div/div/div/div/div/button
         press_visit = driver.find_element(by=By.XPATH, value="/html/body/div[1]/div/div[2]/div/div[1]/div/div/button/div[2]/a").click()
-        assert "SIGN UP" not in driver.page_source
 
-    def editbox_test(self):
+    def test_editbox(self):
         # get driver
         driver = self.driver
         time.sleep(2)
@@ -72,7 +55,7 @@ class KMS_test_class(unittest.TestCase):
         time.sleep(2)
         name_field = driver.find_element(by=By.XPATH, value="/html/body/div[3]/div[3]/div/div[1]/div[1]/div/input")
         time.sleep(2)
-        name_field.send_keys(Keys.COMMAND + 'a' + Keys.BACK_SPACE)
+        name_field.send_keys(Keys.CONTROL + 'a' + Keys.BACK_SPACE)
         time.sleep(2)
         name_field.send_keys("Apartment Uno")
         time.sleep(5)
@@ -81,9 +64,8 @@ class KMS_test_class(unittest.TestCase):
         driver.find_element(by=By.XPATH, value="/html/body/div[3]/div[3]/div/div[2]/button[2]").click()
         time.sleep(2)
         assert "Apartment Uno" in driver.page_source
-        return
 
-    def overview_test(self):
+    def test_overview(self):
         # get driver
         driver = self.driver
         # get python.org using selenium
@@ -98,7 +80,7 @@ class KMS_test_class(unittest.TestCase):
         time.sleep(2)
 
         #Clears text in element
-        driver.find_element(by=By.XPATH, value="/html/body/div[3]/div[3]/div/div[1]/div[2]/div/textarea[1]").send_keys(Keys.COMMAND + 'a' + Keys.BACK_SPACE)
+        driver.find_element(by=By.XPATH, value="/html/body/div[3]/div[3]/div/div[1]/div[2]/div/textarea[1]").send_keys(Keys.CONTROL + 'a' + Keys.BACK_SPACE)
         time.sleep(2)
 
         # change text in  /html/body/div[3]/div[3]/div/div/div/div[2]/div/input to "Worst view ever"
@@ -106,20 +88,20 @@ class KMS_test_class(unittest.TestCase):
         time.sleep(2)
 
         #change_checkin_time in /html/body/div[3]/div[3]/div/div/div/div[4]/div/input to 10:00
-        driver.find_element(by=By.XPATH, value="/html/body/div[3]/div[3]/div/div[1]/div[4]/div[1]/div/input").send_keys(Keys.COMMAND + 'a' + Keys.BACK_SPACE)
+        driver.find_element(by=By.XPATH, value="/html/body/div[3]/div[3]/div/div[1]/div[4]/div[1]/div/input").send_keys(Keys.CONTROL + 'a' + Keys.BACK_SPACE)
         time.sleep(2)
 
         change_checkin_time = driver.find_element(by=By.XPATH, value="/html/body/div[3]/div[3]/div/div[1]/div[4]/div[1]/div/input").send_keys("10:00")
         time.sleep(2)
 
         #change check out time to 14:00 in //*[@id=":r6l:"]
-        driver.find_element(by=By.XPATH, value="/html/body/div[3]/div[3]/div/div[1]/div[4]/div[2]/div/input").send_keys(Keys.COMMAND + 'a' + Keys.BACK_SPACE)
+        driver.find_element(by=By.XPATH, value="/html/body/div[3]/div[3]/div/div[1]/div[4]/div[2]/div/input").send_keys(Keys.CONTROL + 'a' + Keys.BACK_SPACE)
         time.sleep(2)
 
         change_time_out = driver.find_element(by=By.XPATH, value="/html/body/div[3]/div[3]/div/div[1]/div[4]/div[2]/div/input").send_keys("14:00")
         time.sleep(2)
 
-        #click on /html/body/div[3]/div[3]/div/div/div/div[6] 
+        #click on /html/body/div[3]/div[3]/div/div/div/div[6]
         driver.find_element(by=By.XPATH, value="/html/body/div[3]/div[3]/div/div[1]/div[5]/div[2]/canvas").click()
         time.sleep(2)
 
@@ -128,140 +110,158 @@ class KMS_test_class(unittest.TestCase):
         time.sleep(2)
 
         assert "Overview" in driver.page_source
-        return
 
-    def new_booking_test(self):
+
+    def test_new_booking_blocking_form_1(self):
         #get driver
         driver = self.driver
 
-        # Press new_booking //*[@id="root"]/div/div[2]/div/div[1]/div[2]/div/button[1]
+        # Press new_booking_button
         press_new_booking = driver.find_element(by=By.XPATH, value="/html/body/div[1]/div/div[2]/div/div[1]/div[2]/div/button[1]").click()
-        time.sleep(2)
-
-        # enter "MichellTest" in /html/body/div[3]/div[3]/div/div[1]/div[1]/div[1]/div/input
-        driver.find_element(by=By.XPATH, value="/html/body/div[3]/div[3]/div/div[1]/form/div[1]/div/input").send_keys("Michell Test")
-        time.sleep(2)
-
-        # enter_email in /html/body/div[3]/div[3]/div/div[1]/div[1]/div[2]/div/input to "mi.cho123@hotmail.com"
-        driver.find_element(by=By.XPATH, value="/html/body/div[3]/div[3]/div/div[1]/form/div[2]/div/input").send_keys("mi.cho123@hotmail.com")
-        time.sleep(1)
-
-        # enter_guest_message /html/body/div[3]/div[3]/div/div[1]/div[1]/div[3]/div "Welcome to KMS"
-        driver.find_element(by=By.XPATH, value="/html/body/div[3]/div[3]/div/div[1]/form/div[3]/div/textarea[1]").send_keys("Welcome to KMS")
-        time.sleep(1)
-
-        # press /html/body/div[3]/div[3]/div/div[1]/div[1]/div[4]/div/div
-        driver.find_element(by=By.XPATH, value="/html/body/div[3]/div[3]/div/div[1]/form/div[4]/div/div").click()
-        time.sleep(1)
-
-        #press /html/body/div[4]/div[3]/ul/li[2]
-        driver.find_element(by=By.XPATH, value="/html/body/div[4]/div[3]/ul/li[2]").click()
-        time.sleep(1)
-        
-        # Clear checkin-time space
-        driver.find_element(by=By.XPATH, value="/html/body/div[3]/div[3]/div/div[1]/form/div[5]/div[1]/div/input").send_keys(Keys.COMMAND + 'a' + Keys.BACK_SPACE)
-        time.sleep(2)
-
-        #enter checkin time in /html/body/div[3]/div[3]/div/div[1]/div[1]/div[5]/div[1]/div/input to 1100
-        driver.find_element(by=By.XPATH, value="/html/body/div[3]/div[3]/div/div[1]/form/div[5]/div[1]/div/input").send_keys("1100")
-        time.sleep(2)
-
-        #Clear checkout time in /html/body/div[3]/div[3]/div/div[1]/div[1]/div[5]/div[2]/div/input
-        driver.find_element(by=By.XPATH, value="/html/body/div[3]/div[3]/div/div[1]/form/div[5]/div[2]/div/input").send_keys(Keys.COMMAND + 'a' + Keys.BACK_SPACE)
-        time.sleep(2)
-
-        #enter checkout time in /html/body/div[3]/div[3]/div/div[1]/div[1]/div[5]/div[2]/div/input to 1400
-        driver.find_element(by=By.XPATH, value="/html/body/div[3]/div[3]/div/div[1]/form/div[5]/div[2]/div/input").send_keys("1400")
-        time.sleep(2)
-        # press select = /html/body/div[3]/div[3]/div/div[1]/div[2]/div/div[2]/div[2]/div[2]/span/span[1]/select
-        select = Select(driver.find_element_by_xpath("/html/body/div[3]/div[3]/div/div[1]/form/div[6]/div[2]/div[2]/div[2]/span/span[1]/select"))
-
-        #click /html/body/div[3]/div[3]/div/div[1]/div[2]/div/div[2]/div[2]/div[2]/span/span[1]/select
-        driver.find_element_by_xpath("/html/body/div[3]/div[3]/div/div[1]/form/div[6]/div[2]/div[2]/div[2]/span/span[1]/select").click()
-        time.sleep(2)
-
-        #Select by text "May"
-        select.select_by_visible_text("June")
-        time.sleep(2)
-
-        #press day /html/body/div[3]/div[3]/div/div[1]/div[2]/div/div[2]/div[2]/div[3]/div[1]/div[3]/button[18]/span[2]
-        driver.find_element(by=By.XPATH, value="/html/body/div[3]/div[3]/div/div[1]/form/div[6]/div[2]/div[2]/div[3]/div[1]/div[3]/button[20]").click()
-        time.sleep(5)
-
-        #press confirm_booking /html/body/div[3]/div[3]/div/div[2]/button
+        time.sleep(3)
+        #press confirm_booking-button
         driver.find_element(by=By.XPATH, value="/html/body/div[3]/div[3]/div/div[2]/button").click()
-        
-        time.sleep(2)
-        #Assert NEW BOOKING
-        return
-        assert "CONFIRM BOOKING" in driver.page_source
+        actualTitle = driver.title
+        print("Current title of page " + actualTitle)
+        #expectedTitle = "View bookings"
+        expectedTitle = "New booking"
+        print("Expected title " + expectedTitle)
+        assert(expectedTitle == actualTitle)
 
-    #Marius test av new_booking
-    def new_booking_form_test(self):
+    def test_new_booking_blocking_form_2(self):
         #get driver
         driver = self.driver
 
-        # Press new_booking //*[@id="root"]/div/div[2]/div/div[1]/div[2]/div/button[1]
+        # Press new_booking_button
         press_new_booking = driver.find_element(by=By.XPATH, value="/html/body/div[1]/div/div[2]/div/div[1]/div[2]/div/button[1]").click()
-        time.sleep(2)
+        time.sleep(3)
+        # enter test data into username form
+        username_form = driver.find_element(by=By.XPATH, value="/html/body/div[3]/div[3]/div/div[1]/form/div[1]/div/input").send_keys("Testgrabben Testare")
+        time.sleep(1)
+        #press confirm_booking-button
+        driver.find_element(by=By.XPATH, value="/html/body/div[3]/div[3]/div/div[2]/button").click()
+        actualTitle = driver.title
+        print("Current title of page " + actualTitle)
+        #expectedTitle = "View bookings"
+        expectedTitle = "New booking"
+        print("Expected title " + expectedTitle)
 
-        # enter "MichellTest" in /html/body/div[3]/div[3]/div/div[1]/div[1]/div[1]/div/input
-        username_form = driver.find_element(by=By.XPATH, value="/html/body/div[3]/div[3]/div/div[1]/form/div[1]/div/input").send_keys("Michell Test")
-        time.sleep(2)
+        assert(expectedTitle == actualTitle)
 
-        # enter_email in /html/body/div[3]/div[3]/div/div[1]/div[1]/div[2]/div/input to "mi.cho123@hotmail.com"
-        email_form = driver.find_element(by=By.XPATH, value="/html/body/div[3]/div[3]/div/div[1]/form/div[2]/div/input").send_keys("mi.cho123@hotmail.com")
+    def test_new_booking_blocking_form_3(self):
+        #get driver
+        driver = self.driver
+
+        # Press new_booking_button
+        press_new_booking = driver.find_element(by=By.XPATH, value="/html/body/div[1]/div/div[2]/div/div[1]/div[2]/div/button[1]").click()
+        time.sleep(3)
+        # enter test data into username form
+        username_form = driver.find_element(by=By.XPATH, value="/html/body/div[3]/div[3]/div/div[1]/form/div[1]/div/input").send_keys("Testgrabben Testare")
+        time.sleep(1)
+        # enter_email in email form
+        email_form = driver.find_element(by=By.XPATH, value="/html/body/div[3]/div[3]/div/div[1]/form/div[2]/div/input").send_keys("testmail@test123.com")
+        time.sleep(1)
+        #press confirm_booking-button
+        driver.find_element(by=By.XPATH, value="/html/body/div[3]/div[3]/div/div[2]/button").click()
+        actualTitle = driver.title
+        print("Current title of page " + actualTitle)
+        #expectedTitle = "View bookings"
+        expectedTitle = "New booking"
+        print("Expected title " + expectedTitle)
+
+        assert(expectedTitle == actualTitle)
+
+    def test_new_booking_blocking_form_4(self):
+        #get driver
+        driver = self.driver
+
+        # Press new_booking_button
+        press_new_booking = driver.find_element(by=By.XPATH, value="/html/body/div[1]/div/div[2]/div/div[1]/div[2]/div/button[1]").click()
+        time.sleep(3)
+        # enter test data into username form
+        username_form = driver.find_element(by=By.XPATH, value="/html/body/div[3]/div[3]/div/div[1]/form/div[1]/div/input").send_keys("Testgrabben Testare")
+        time.sleep(1)
+        # enter_email in email form
+        email_form = driver.find_element(by=By.XPATH, value="/html/body/div[3]/div[3]/div/div[1]/form/div[2]/div/input").send_keys("testmail@test123.com")
         time.sleep(1)
 
-        # enter_guest_message /html/body/div[3]/div[3]/div/div[1]/div[1]/div[3]/div "Welcome to KMS"
-        guest_message_form = driver.find_element(by=By.XPATH, value="/html/body/div[3]/div[3]/div/div[1]/form/div[3]/div/textarea[1]").send_keys("Welcome to KMS")
+        # enter_guest_message in form
+        guest_message_form = driver.find_element(by=By.XPATH, value="/html/body/div[3]/div[3]/div/div[1]/form/div[3]/div/textarea[1]").send_keys("Yes this is the best room")
+        time.sleep(1)
+        #press confirm_booking-button
+        driver.find_element(by=By.XPATH, value="/html/body/div[3]/div[3]/div/div[2]/button").click()
+        actualTitle = driver.title
+        print("Current title of page " + actualTitle)
+        #expectedTitle = "View bookings"
+        expectedTitle = "New booking"
+        print("Expected title " + expectedTitle)
+
+        assert(expectedTitle == actualTitle)
+
+    def test_new_booking_blocking_form_final(self):
+        #get driver
+        driver = self.driver
+
+        # Press new_booking_button
+        press_new_booking = driver.find_element(by=By.XPATH, value="/html/body/div[1]/div/div[2]/div/div[1]/div[2]/div/button[1]").click()
+        time.sleep(1)
+        # enter test data into username form
+        username_form = driver.find_element(by=By.XPATH, value="/html/body/div[3]/div[3]/div/div[1]/form/div[1]/div/input").send_keys("Testgrabben Testare")
+        time.sleep(1)
+        # enter_email in email form
+        email_form = driver.find_element(by=By.XPATH, value="/html/body/div[3]/div[3]/div/div[1]/form/div[2]/div/input").send_keys("testmail@test123.com")
         time.sleep(1)
 
-        # press /html/body/div[3]/div[3]/div/div[1]/div[1]/div[4]/div/div
+        # enter_guest_message in form
+        guest_message_form = driver.find_element(by=By.XPATH, value="/html/body/div[3]/div[3]/div/div[1]/form/div[3]/div/textarea[1]").send_keys("Yes this is the best room")
+        time.sleep(1)
+
+        # press
         driver.find_element(by=By.XPATH, value="/html/body/div[3]/div[3]/div/div[1]/form/div[4]/div/div").click()
         time.sleep(1)
 
-        #press /html/body/div[4]/div[3]/ul/li[2]
+        #press
         driver.find_element(by=By.XPATH, value="/html/body/div[4]/div[3]/ul/li[2]").click()
         time.sleep(1)
 
-        # Clear checkin-time space
+        # Clear checkin-timebox
         driver.find_element(by=By.XPATH, value="/html/body/div[3]/div[3]/div/div[1]/form/div[5]/div[1]/div/input").send_keys(Keys.COMMAND + 'a' + Keys.BACK_SPACE)
-        time.sleep(2)
+        time.sleep(1)
 
-        #enter checkin time in /html/body/div[3]/div[3]/div/div[1]/div[1]/div[5]/div[1]/div/input to 1100
+        #enter checkin time
         driver.find_element(by=By.XPATH, value="/html/body/div[3]/div[3]/div/div[1]/form/div[5]/div[1]/div/input").send_keys("1100")
-        time.sleep(2)
+        time.sleep(1)
 
-        #Clear checkout time in /html/body/div[3]/div[3]/div/div[1]/div[1]/div[5]/div[2]/div/input
+        #Clear checkout timebox
         driver.find_element(by=By.XPATH, value="/html/body/div[3]/div[3]/div/div[1]/form/div[5]/div[2]/div/input").send_keys(Keys.COMMAND + 'a' + Keys.BACK_SPACE)
-        time.sleep(2)
+        time.sleep(1)
 
-        #enter checkout time in /html/body/div[3]/div[3]/div/div[1]/div[1]/div[5]/div[2]/div/input to 1400
+        #enter checkout time
         driver.find_element(by=By.XPATH, value="/html/body/div[3]/div[3]/div/div[1]/form/div[5]/div[2]/div/input").send_keys("1400")
-        time.sleep(2)
-        # press select = /html/body/div[3]/div[3]/div/div[1]/div[2]/div/div[2]/div[2]/div[2]/span/span[1]/select
+        time.sleep(1)
+
         select = Select(driver.find_element_by_xpath("/html/body/div[3]/div[3]/div/div[1]/form/div[6]/div[2]/div[2]/div[2]/span/span[1]/select"))
 
-        #click /html/body/div[3]/div[3]/div/div[1]/div[2]/div/div[2]/div[2]/div[2]/span/span[1]/select
         driver.find_element_by_xpath("/html/body/div[3]/div[3]/div/div[1]/form/div[6]/div[2]/div[2]/div[2]/span/span[1]/select").click()
-        time.sleep(2)
+        time.sleep(1)
 
-        #Select by text "May"
+        #Select by text "june"
         select.select_by_visible_text("June")
-        time.sleep(2)
+        time.sleep(1)
 
         #press day /html/body/div[3]/div[3]/div/div[1]/div[2]/div/div[2]/div[2]/div[3]/div[1]/div[3]/button[18]/span[2]
         driver.find_element(by=By.XPATH, value="/html/body/div[3]/div[3]/div/div[1]/form/div[6]/div[2]/div[2]/div[3]/div[1]/div[3]/button[20]").click()
-        time.sleep(5)
+        time.sleep(1)
 
-        #press confirm_booking /html/body/div[3]/div[3]/div/div[2]/button
         driver.find_element(by=By.XPATH, value="/html/body/div[3]/div[3]/div/div[2]/button").click()
-
         time.sleep(2)
-        #Assert NEW BOOKING
-        return
-        assert "CONFIRM BOOKING" in driver.page_source
+        actualTitle = driver.title
+        print("Current title of page " + actualTitle)
+        expectedTitle = "View bookings"
+        print("Expected title " + expectedTitle)
+
+        assert(expectedTitle == actualTitle)
+
 
     # cleanup method called after every test performed
     def tearDown(self):
